b7431bda73f7ef46cd701e58af7ce6ccf1674fef
WhiteZombieUA <mshvets@emp.org.ua>
2015-12-09 15:55:08 +0200

    version 1.0

--- www/js/services.js
+++ www/js/services.js
@@ -15,7 +15,7 @@ var forecastioWeather = ['$q', '$resource', '$http', 'FORECASTIO_KEY',
     return {
       //getAtLocation: function(lat, lng) {
       getCurrentWeather: function(lat, lng) {
-        return $http.jsonp(url + lat + ',' + lng + '?callback=JSON_CALLBACK');
+        return $http.jsonp(url + lat + ',' + lng + '?units=si&callback=JSON_CALLBACK');
       }
     }
   }];
@@ -23,13 +23,13 @@ var forecastioWeather = ['$q', '$resource', '$http', 'FORECASTIO_KEY',
 angular.module('starter.services', ['ngResource'])
   .factory('Cities', function() {
     var cities = [
-      { id: 0, name: 'Miami', lat:25.7877 , lgn: 80.2241 },
-      { id: 1, name: 'New York City' ,lat: 40.7127 , lgn: 74.0059 },
-      { id: 2, name: 'London' ,lat:51.5072 , lgn: 1.1275 },
-      { id: 3, name: 'Los Angeles' ,lat: 34.0500 , lgn: 118.2500 },
-      { id: 4, name: 'Dallas' ,lat: 32.7758 , lgn:96.7967  },
-      { id: 5, name: 'Frankfurt' ,lat:50.1117 , lgn: 8.6858 },
-      { id: 6, name: 'New Delhi' ,lat:28.6100 , lgn: 77.2300 }
+      { id: 0, name: 'Miami', lat: 25.7823 , lgn: -80.2310 },
+      { id: 1, name: 'New York City', lat: 40.7059 , lgn: -73.9780 },
+      { id: 2, name: 'London', lat:51.5288, lgn: -0.1015 },
+      { id: 3, name: 'Los Angeles', lat: 34.0210 , lgn: -118.4117 },
+      { id: 4, name: 'Lviv' ,lat: 49.8327 , lgn: 24.0122  },
+      { id: 5, name: 'Frankfurt' ,lat:50.1213 , lgn: 8.6365 },
+      { id: 6, name: 'New Delhi' ,lat:28.6472 , lgn: 77.0932 }
     ];
 
     return {
@@ -45,16 +45,16 @@ angular.module('starter.services', ['ngResource'])
   factory('DataStore', function() {
     //create datastore with default values
     var DataStore = {
-      city:       'Miami',
-      latitude:   25.7877,
-      longitude:  80.2241 };
+      city:       'Lviv',
+      latitude:   49.8444,
+      longitude:  24.0254};
 
     DataStore.setCity = function (value) {
       DataStore.city = value;
     };
 
     DataStore.setLatitude = function (value) {
-      DataStore.longitude = value;
+      DataStore.latitude = value;
     };
 
     DataStore.setLongitude = function (value) {
